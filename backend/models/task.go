bb9869cd54ad3ec2f44a0588ff63c63c359aad36
westfalensgod <westfalensgod@gmail.com>
2018-10-16 21:42:12 +0300

    Update task.go

--- backend/models/task.go
+++ backend/models/task.go
@@ -1,11 +1,11 @@
 package models
 
 type Task struct {
-	ID uint `gorm:"primary_key" json:"id"`
+ID uint `gorm:"primary_key" json:"id"`
 
-	ProjectID   uint   `gorm:"index"`
-	StartDate   string `json:"StartDate"`
-	EndDate     string `json:"EndDate"`
-	Description string `json:"Description"`
-	Image       string `json:"Image"`
+ProjectID   uint   `gorm:"index"`
+StartDate   string `json:"StartDate"`
+EndDate     string `json:"EndDate"`
+Description string `json:"Description"`
+Image       string `json:"Image"`
 }
